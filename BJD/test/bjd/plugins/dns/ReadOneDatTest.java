3cb010a3824c102bedddf65d5312db52b416a824
furuya02 <sin@sapporoworks.ne.jp>
2012-12-24 14:51:10 +0900

    2012.12.24 1450

--- BJD/test/bjd/plugins/dns/ReadOneDatTest.java
+++ BJD/test/bjd/plugins/dns/ReadOneDatTest.java
@@ -5,6 +5,8 @@ import static org.junit.Assert.*;
 
 import java.util.ArrayList;
 
+import junit.framework.Assert;
+
 import org.junit.Test;
 
 import bjd.option.OneDat;
@@ -21,16 +23,16 @@ public final class ReadOneDatTest {
 		//exercise
 		DnsUtil.readOneDat(domainName, oneDat, ar);
 
-		//verify
+		//verify count
 		assertThat(ar.size(), is(2)); //A,PTR
 
-		//A
+		//verify A
 		RrA a = (RrA) ar.get(0);
 		assertThat(a.getDnsType(), is(DnsType.A));
 		assertThat(a.getName(), is("www.aaa.com"));
 		assertThat(a.getIp().toString(), is("192.168.0.1"));
 
-		//PTR
+		//verify PTR
 		RrPtr p = (RrPtr) ar.get(1);
 		assertThat(p.getDnsType(), is(DnsType.Ptr));
 		assertThat(p.getName(), is("www.aaa.com"));
@@ -47,22 +49,36 @@ public final class ReadOneDatTest {
 		//exercise
 		DnsUtil.readOneDat(domainName, oneDat, ar);
 
-		//verify
+		//verify count
 		assertThat(ar.size(), is(2)); //AAAA,PTR
 
-		//AAAA
+		//verify AAAA
 		RrAaaa a = (RrAaaa) ar.get(0);
 		assertThat(a.getDnsType(), is(DnsType.Aaaa));
 		assertThat(a.getName(), is("www.aaa.com"));
 		assertThat(a.getIp().toString(), is("fe80::f509:c5be:437b:3bc5"));
 
-		//PTR
+		//verify PTR
 		RrPtr p = (RrPtr) ar.get(1);
 		assertThat(p.getDnsType(), is(DnsType.Ptr));
 		assertThat(p.getName(), is("www.aaa.com"));
 		assertThat(p.getPtr(), is("5.c.b.3.b.7.3.4.e.b.5.c.9.0.5.f.0.0.0.0.0.0.0.0.0.0.0.0.0.8.e.f.ip6.arpa."));
 
 	}
-	
-	
+
+	@Test
+	public void MXレコードを読み込んだ時_AAAA及びPTRが保存される() throws Exception {
+		Assert.fail("未実装");
+	}
+
+	@Test
+	public void NSレコードを読み込んだ時_AAAA及びPTRが保存される() throws Exception {
+		Assert.fail("未実装");
+	}
+
+	@Test
+	public void CNAMEレコードを読み込んだ時_AAAA及びPTRが保存される() throws Exception {
+		Assert.fail("未実装");
+	}
+
 }
